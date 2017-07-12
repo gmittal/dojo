81181676d27a995e1aa03d21b1b22c06c9576df1
Gautam Mittal <i-gautam@edmodo.com>
2017-07-12 17:47:35 +0000

    Add functional sentence embedding vectors.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Quora Question Pairs Task
 # https://www.kaggle.com/c/quora-question-pairs
 # Written by Gautam Mittal
@@ -5,9 +7,14 @@
 
 import numpy as np
 import tensorflow as tf
-import spacy, json
+import spacy, json, sys, pandas, tqdm
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
 nlp = spacy.load('en')
+glove = {}
+word_probs = {}
+direction = []
 
 def load_glove_vecs(path='glove/glove.840B.300d.json'):
     return json.loads(open(path).read())
@@ -22,7 +29,8 @@ def pca(mat, n):
 def get_word_probs(sent_list):
     total = 0.0
     VOCAB = {}
-    for sentence in sent_list:
+    for i in tqdm.tqdm(range(len(sent_list))):
+        sentence = sent_list[i]
         doc = nlp(unicode(sentence))
         for words in doc:
             total += 1.0
@@ -34,15 +42,15 @@ def get_word_probs(sent_list):
     return VOCAB
 
 def word_weight(word, a=1.0):
-    try word_probs[word]:
-        return a / (a + word_probs[word])
+    try:
+        return a / (a + word_probs[word.lower()])
     except KeyError:
         return 1
 
 def sent_vec(sent):
     doc = nlp(unicode(sent))
     vec = np.zeros(300)
-    for word in doc: vec += word_weight[word.text] * glove[word.text]
+    for word in doc: vec += word_weight(word.text) * np.asarray(glove[word.text.lower()])
     return vec / len(doc)
 
 def feature_vec(sent, fpc):
@@ -53,14 +61,29 @@ def vec_sim(u, v):
     return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
 def init():
-    questions = ["Hello world", "Who are you, hello"]
-
     global glove, word_probs, direction
+    
+    print "Loading Training Data..."
+    train_data = pandas.read_csv('data/train.csv')
+    questions = list(train_data['question1'].values) + list(train_data['question2'].values)
+
+    print "Loading Glove Vectors..."
     glove = load_glove_vecs()
-    word_probs = get_word_probs(questions)
 
+    print "Loading word weights..."
+    try:
+        word_probs = json.loads(open('data/weights.json').read())
+    except IOError:
+        word_probs = get_word_probs(map(lambda x: unicode(x), questions))
+        with open('data/weights.json', 'w') as outfile:
+                json.dump(word_probs, outfile)
 
-    direction =
+    train_sent_matrix = np.asarray(sent_vec(questions[0]))
+    for i in tqdm.tqdm(range(1, len(questions))):
+        np.column_stack((train_sent_matrix, np.asarray(sent_vec(questions[i]))))
+
+    print train_sent_matrix.shape
+    
 
 if __name__ == "__main__":
     init()
