e93b5e84e92b678a31001d78dac6602b42cbf1c2
Gautam Mittal <gautam@mittal.net>
2018-06-07 11:01:20 -0700

    Create main.py with basic dense model.

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.callbacks import TensorBoard
+
+tensorboard = TensorBoard(log_dir="logs/")
+
+data = pd.read_csv('data/train.csv')
+data = data.drop(columns=['Cabin', 'Age', 'Ticket', 'Embarked'])
+print(data)
+
+
+# model = Sequential()
+# model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), strides=(1, 1),
+#                  activation='relu',
+#                  input_shape=(28, 28, 1)))
+# model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+# model.add(tf.keras.layers.Dropout(0.2))
+#
+# model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+# model.add(tf.keras.layers.Dropout(0.2))
+#
+# model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+# model.add(tf.keras.layers.Dropout(0.2))
+#
+# model.add(tf.keras.layers.Flatten())
+# model.add(tf.keras.layers.Dense(128, activation='relu'))
+# model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#
+# model.fit(train_images, train_labels, epochs=10, batch_size=256, callbacks=[tensorboard])
+# loss, accuracy = model.evaluate(test_images, test_labels)
+# print(accuracy)
