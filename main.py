0bad989962642a36d4ffd2bb4e34e0531a954518
Gautam Mittal <gautam@mittal.net>
2018-06-07 14:12:19 -0700

    Fix age — an important feature

--- main.py
+++ main.py
@@ -10,14 +10,21 @@ tensorboard = TensorBoard(log_dir="logs/")
 TRAIN_SPLIT = 0.75
 np.random.seed(0)
 
-data = pd.read_csv('data/train.csv')
-labels = data['Survived']
-data['Dependents'] = data['Parch'] + data['SibSp']
-features = data.drop(columns=['Name', 'Cabin', 'Age', 'Ticket', 'Embarked', 'PassengerId', 'Survived', 'Parch', 'SibSp'])
+features = pd.read_csv('data/train.csv')
+labels = features['Survived']
+features['Dependents'] = features['Parch'] + features['SibSp']
 features['Female'] = list(map(lambda x: 1 if x == 'female' else 0, features['Sex'])) # Sex should be mapped to binary value
 features['Male'] = list(map(lambda x: 1 if x == 'male' else 0, features['Sex']))
-features = features.drop(columns=['Sex'])
 features['Alone'] = list(map(lambda x: 1 if x == 0 else 0, features['Dependents']))
+features['Class1'] = list(map(lambda x: 1 if x == 1 else 0, features['Pclass']))
+features['Class2'] = list(map(lambda x: 1 if x == 2 else 0, features['Pclass']))
+features['Class3'] = list(map(lambda x: 1 if x == 3 else 0, features['Pclass']))
+median_age = np.median(features['Age'][np.isfinite(features['Age'])].values)
+features['Age'] = list(map(lambda x: median_age if np.isnan(x) else x, features['Age']))
+features = features.drop(columns=['Name', 'Sex', 'Pclass', 'Cabin', 'Ticket', 'Embarked', 'PassengerId', 'Survived', 'Parch', 'SibSp'])
+
+
+print(features)
 
 # Prepare training and validation splits
 msk = np.random.rand(len(features)) < TRAIN_SPLIT
