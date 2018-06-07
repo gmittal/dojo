93223e54497f1eaad28ead167d8d51d137a70d48
Gautam Mittal <gautam@mittal.net>
2018-06-07 13:24:24 -0700

    Messing with more features.

--- main.py
+++ main.py
@@ -7,21 +7,17 @@ from tensorflow.python.keras.callbacks import TensorBoard
 
 tensorboard = TensorBoard(log_dir="logs/")
 
-TRAIN_SPLIT = 0.7
+TRAIN_SPLIT = 0.75
 np.random.seed(0)
 
 data = pd.read_csv('data/train.csv')
 labels = data['Survived']
 data['Dependents'] = data['Parch'] + data['SibSp']
 features = data.drop(columns=['Name', 'Cabin', 'Age', 'Ticket', 'Embarked', 'PassengerId', 'Survived', 'Parch', 'SibSp'])
-
-# Data normalization
-features['Sex'] = list(map(lambda x: 1 if x == 'female' else 0, features['Sex'])) # Sex should be mapped to binary value
-
-mean = features.mean(axis=0)
-features -= mean
-std = features.std(axis=0)
-features /= std
+features['Female'] = list(map(lambda x: 1 if x == 'female' else 0, features['Sex'])) # Sex should be mapped to binary value
+features['Male'] = list(map(lambda x: 1 if x == 'male' else 0, features['Sex']))
+features = features.drop(columns=['Sex'])
+features['Alone'] = list(map(lambda x: 1 if x == 0 else 0, features['Dependents']))
 
 # Prepare training and validation splits
 msk = np.random.rand(len(features)) < TRAIN_SPLIT
@@ -34,14 +30,16 @@ train_labels = labels[msk].values
 val_data = features[~msk].values
 val_labels = labels[~msk].values
 
-print(train_data)
-print(train_labels)
-
 model = Sequential()
-model.add(Dense(128, input_shape=(4,), activation='sigmoid'))
-model.add(Dense(1, activation=tf.nn.softmax))
+model.add(Dense(100, input_shape=(6,), activation='relu'))
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(700, activation='relu'))
+model.add(Dense(500, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-model.fit(train_data, train_labels, validation_data=(val_data, val_labels), epochs=10, batch_size=256, callbacks=[tensorboard])
+model.fit(train_data, train_labels, validation_data=(val_data, val_labels), epochs=10, shuffle=True, callbacks=[tensorboard])
 # loss, accuracy = model.evaluate(test_images, test_labels)
 # print(accuracy)
