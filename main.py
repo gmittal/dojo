eebb994323be678cadd104e5d135efabf55e908b
Gautam Mittal <gautam@mittal.net>
2018-06-18 18:22:37 -0700

    Add basic CNN architecture.

---
+++ main.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten, BatchNormalization
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+import pickle
+
+tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs/")
+
+# Load the training data
+(train_data, train_labels), (test_data, test_labels) = tf.keras.datasets.cifar10.load_data()
+train_data = train_data.astype('float32')
+test_data = test_data.astype('float32')
+train_data /= 255
+test_data /= 255
+train_labels = tf.keras.utils.to_categorical(train_labels, 10)
+test_labels = tf.keras.utils.to_categorical(test_labels, 10)
+
+# Data augmentation
+generator = ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    rotation_range=10,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    vertical_flip=False
+)
+
+generator.fit(train_data)
+
+# Build a huge model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1),
+                 activation='relu',
+                 input_shape=(32, 32, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization(axis=-1))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization(axis=-1))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Dense(10, activation=tf.nn.softmax))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+# Train the huge model
+# model.fit(train_images, train_labels, validation_split=0.2, epochs=10, shuffle=True, batch_size=256, callbacks=[tensorboard])
+model.fit_generator(generator.flow(train_data, train_labels, batch_size=64), epochs=20, callbacks=[tensorboard])
+
+# # Predict some image classes
+# results = model.predict(test_data)
+# results = np.argmax(results, axis=1)
+# results = pd.Series(results,name="Label")
+# prediction = pd.concat([pd.Series(range(1, 28001), name = "ImageId"),results], axis=1)
+# prediction.to_csv("submission.csv", index=False)
