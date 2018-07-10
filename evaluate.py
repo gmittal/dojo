09b4fddac5dab838110cd8c6737d0f4a2c69a1d8
Gautam Mittal <gautam@mittal.net>
2018-07-09 23:32:42 -0700

    Add command line argument for custom comment evaluation

--- evaluate.py
+++ evaluate.py
@@ -6,6 +6,12 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 from util.tokenizer_helpers import *
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Try the toxic comment classifier.')
+parser.add_argument('-t', action='store', dest='test', default=True, help='Make predictions on test.csv and output a submission.csv')
+parser.add_argument('-c', action='store', dest='comment', type=str, help='Classify an example comment')
+args = parser.parse_args()
+
 # Rebuild saved tokenizer
 tokenizer = load_tokenizer('save/tokenizer.pickle')
 
@@ -16,6 +22,21 @@ test_tokens = tokenizer.texts_to_sequences(test_sent)
 test = pad_sequences(test_tokens, maxlen=300)
 
 model = load_model('save/model.h5')
+
+if args.comment:
+    args.test = False
+    comment_tokens = tokenizer.texts_to_sequences([args.comment])
+    comment_vec = pad_sequences(comment_tokens, maxlen=300)
+    predictions = model.predict(comment_vec)[0]
+    print('Toxic: {0:.2f}%'.format(predictions[0] * 100))
+    print('Severe Toxic: {0:.2f}%'.format(predictions[1] * 100))
+    print('Obscene: {0:.2f}%'.format(predictions[2] * 100))
+    print('Threat: {0:.2f}%'.format(predictions[3] * 100))
+    print('Insult: {0:.2f}%'.format(predictions[4] * 100))
+    print('Identity Hate: {0:.2f}%'.format(predictions[5] * 100))
+
+if not args.test: # Terminate the program early if we're not creating test predictions
+    sys.exit()
 
 submission = pd.read_csv('data/sample_submission.csv')
 y_pred = model.predict(test, batch_size=1024)
